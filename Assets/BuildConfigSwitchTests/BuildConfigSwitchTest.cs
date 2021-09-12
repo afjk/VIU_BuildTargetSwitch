7c3bc3d71fa18effc595e353d8650ab7d25ee83b
afjk <afjk01@gmail.com>
2021-09-12 22:03:49 +0900

    Add Test

--- Assets/BuildConfigSwitchTests/BuildConfigSwitchTest.cs
+++ Assets/BuildConfigSwitchTests/BuildConfigSwitchTest.cs
@@ -1,9 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using AFJK.BuildConfigSwitch;
+﻿using AFJK.BuildConfigSwitch;
 using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
-using UnityEngine.TestTools;
+using UnityEngine.UIElements;
+
+/*
+ * ToDo:
+ * * SerializeFieldに設定したScriptableObjectが参照出来る
+ * * ScriptableObjectを.assetファイルからロード出来る
+ * * BuildConfigSwitcherクラスを生成出来る
+ * * 
+ */
 
 namespace Tests
 {
@@ -18,5 +25,22 @@ namespace Tests
             Assert.NotNull(testScriptableObject);
             Assert.AreEqual(testScriptableObject.addDefines[0], "TestDefine1");
         }
+
+        [Test]
+        public void LoadScriptableObjectTest()
+        {
+            var scriptableObject = AssetDatabase.LoadAssetAtPath<BuildConfigScriptableObject>("Assets/BuildConfigSwitchTests/TestData/BuildConfigTest.asset");
+            Assert.NotNull(scriptableObject);
+            Assert.AreEqual(scriptableObject.addDefines[0], "TestDefine1");
+        }
+
+        [Test]
+        public void NewBuildConfigTest()
+        {
+            var buildConfig = new BuildConfigSwitcher(testScriptableObject);
+            
+            Assert.NotNull(buildConfig);
+        }
+        
     }
 }

1a1fcf99a247e9470b02ed28678bcdaed15ff859
afjk <afjk01@gmail.com>
2021-09-13 08:05:15 +0900

    ApplyDefineSymbols実装

--- Assets/BuildConfigSwitch/Editor/BuildConfigSwitcher.cs
+++ Assets/BuildConfigSwitch/Editor/BuildConfigSwitcher.cs
@@ -1,11 +1,56 @@
-﻿namespace AFJK.BuildConfigSwitch
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework.Constraints;
+using UnityEditor;
+using UnityEngine;
+
+namespace AFJK.BuildConfigSwitch
 {
     public class BuildConfigSwitcher
     {
-        readonly BuildConfigScriptableObject buildConfigScriptableObject;
-        public BuildConfigSwitcher(BuildConfigScriptableObject buildConfigScriptableObject)
+        readonly BuildConfigScriptableObject buildParam;
+        public BuildConfigSwitcher(BuildConfigScriptableObject buildParam)
         {
-            this.buildConfigScriptableObject = buildConfigScriptableObject;
+            this.buildParam = buildParam;
+        }
+
+        public void ApplyDefineSymbols()
+        {
+            var defineSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(GetBuildTarget(buildParam.buildTarget));
+
+            var defineSymbolList = defineSymbols.Split(';').ToList();
+
+            // Define追加
+            if (buildParam.addDefines!=null &&  buildParam.addDefines.Length > 0)
+            {
+                defineSymbolList.AddRange(buildParam.addDefines.ToList().Distinct());
+            }
+            
+            // Define削除
+            if (buildParam.removeDefines != null && buildParam.removeDefines.Length > 0)
+            {
+                defineSymbolList.RemoveAll(x => buildParam.removeDefines.Contains(x));
+            }
+
+            var newDefineSymbols = string.Join(";", defineSymbolList.Distinct());
+            
+            Debug.Log(newDefineSymbols);
+            
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(GetBuildTarget(buildParam.buildTarget),newDefineSymbols );
+        }
+
+        private BuildTargetGroup GetBuildTarget(BuildTarget buildTarget)
+        {
+            switch (buildTarget)
+            {
+                case BuildTarget.Android:
+                    return BuildTargetGroup.Android;
+                case BuildTarget.StandaloneWindows64:
+                    return BuildTargetGroup.Standalone;
+                default:
+                    throw new Exception($"Unknown BuildTarget:{buildTarget}");
+            }
         }
     }
 }
--- Assets/BuildConfigSwitchTests/BuildConfigSwitchTest.cs
+++ Assets/BuildConfigSwitchTests/BuildConfigSwitchTest.cs
@@ -1,4 +1,5 @@
-﻿using AFJK.BuildConfigSwitch;
+﻿using System.Linq;
+using AFJK.BuildConfigSwitch;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
@@ -6,10 +7,17 @@ using UnityEngine.UIElements;
 
 /*
  * ToDo:
- * * SerializeFieldに設定したScriptableObjectが参照出来る
- * * ScriptableObjectを.assetファイルからロード出来る
- * * BuildConfigSwitcherクラスを生成出来る
- * * 
+ * * SerializeFieldに設定したScriptableObjectの参照
+ * * ScriptableObjectを.assetファイルからロード
+ * * BuildConfigSwitcherクラスの生成
+ * * 指定Defineの追加
+ * * 設定Defineの削除
+ * * 指定Packageのインストール
+ * * 指定PackageのRemove
+ * * 指定フォルダの無効化
+ * * 無効化フォルダの有効化
+ * * ProjectSettinsへの変更反映
+ * * 指定VIU Settings Support Deviceのチェック
  */
 
 namespace Tests
@@ -41,6 +49,42 @@ namespace Tests
             
             Assert.NotNull(buildConfig);
         }
-        
+
+        [Test]
+        public void AddDefineTest()
+        {
+            var testConfigSO = ScriptableObject.CreateInstance<BuildConfigScriptableObject>();
+            testConfigSO.buildTarget = BuildTarget.Android;
+            testConfigSO.addDefines = new[] {"TestDefine1, TestDefine2"};
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android,"" );
+
+            var bulidConfig = new BuildConfigSwitcher(testConfigSO);
+            bulidConfig.ApplyDefineSymbols();
+
+            var defineSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android);
+            var defineSymbolList = defineSymbols.Split(';');
+
+            Assert.IsTrue(defineSymbolList.Contains("TestDefine1"));
+            Assert.IsTrue(defineSymbolList.Contains("TestDefine2"));
+        }
+
+        [Test]
+        public void RemoveDefineTest()
+        {
+            var testConfigSO = ScriptableObject.CreateInstance<BuildConfigScriptableObject>();
+            testConfigSO.buildTarget = BuildTarget.StandaloneWindows64;
+            testConfigSO.removeDefines = new[] {"TestDefine2"};
+            
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone,"TestDefine1;TestDefine2");
+            
+            var buildConfig = new BuildConfigSwitcher(testConfigSO);
+            buildConfig.ApplyDefineSymbols();
+            
+            var defineSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone);
+            var defineSymbolList = defineSymbols.Split(';');
+            
+            Assert.IsTrue(defineSymbolList.Contains("TestDefine1"));
+            Assert.IsFalse(defineSymbolList.Contains("TestDefine2"));
+        }
     }
 }

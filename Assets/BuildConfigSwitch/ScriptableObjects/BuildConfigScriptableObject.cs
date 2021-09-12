cca11e132b020b528e25874eee8fa080b750ef47
afjk <afjk01@gmail.com>
2021-09-12 21:14:45 +0900

    Add Build Config Scriptable Object

---
+++ Assets/BuildConfigSwitch/ScriptableObjects/BuildConfigScriptableObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace AFJK.BuildConfigSwitch
+{
+    [CreateAssetMenu(fileName = "BuildConfig", menuName = "ScriptableObjects/BuildConfig", order = 1)]
+    public class BuildConfigScriptableObject : ScriptableObject
+    {
+        public string[] addDefines;
+        public string[] removeDefines;
+        public string[] addPackages;
+        public string[] removePackages;
+        public string[] evacuateFiles;
+        public bool legacyVRSupported = false;
+        public string androidManifestPath;
+        public BuildTarget buildTarget = BuildTarget.Android;
+        public UnityEngine.Rendering.GraphicsDeviceType[] graphicsDeviceTypes;
+        public ScriptingImplementation backend = ScriptingImplementation.IL2CPP;
+        public AndroidSdkVersions minSdkVersion = AndroidSdkVersions.AndroidApiLevel26;
+        public AndroidSdkVersions targetSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
+        public AndroidArchitecture androidArchitecture = AndroidArchitecture.ARM64;
+        public bool AppBundle_aab = false;
+    }
+}
